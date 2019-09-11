93efb5741af313cc3eae13242fa7cff383d832d0
LucasTissierTeaam <lucastissierpro@gmail.com>
2019-09-11 12:33:07 +0200

    fix

--- server/environment/production.js
+++ server/environment/production.js
@@ -1,9 +1,7 @@
-const path = require('path');
-
 const pass = encodeURIComponent('2VqrS7tIyFFcoNlv');
 
 module.exports= {
     dbUrl: 'mongodb+srv://lucasSBTXappClient:'+pass+'@cluster0-xppuo.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    cert: path.join( __dirname, ''),
-    key: path.join( __dirname, '')
+    cert: '/etc/letsencrypt/live/www.app-sbtx.site/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.app-sbtx.site/privkey.pem'
 }
